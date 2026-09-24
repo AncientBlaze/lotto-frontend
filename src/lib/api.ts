f7763982bf5340f41fd56274c9a052d128731900
AncientBlaze <saikata62@gmail.com>
2026-09-24 07:48:44 +0000

    updated api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: 'http://162.35.117.146',
   headers: {
     'Content-Type': 'application/json',
   },
